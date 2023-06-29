b5bd83f22725aa18310ccdcb283e8364ffbbc559
josh <joshuastolle445@gmail.com>
2023-06-29 10:53:13 +0100

    added name to Theme Object

--- backend/src/main/java/de/neuefische/backend/Theme.java
+++ backend/src/main/java/de/neuefische/backend/Theme.java
@@ -9,13 +9,15 @@ import java.util.UUID;
 public class Theme {
 
     private final String id;
+    private String name;
     private String springUrl;
     private String summerUrl;
     private String autumnUrl;
     private String winterUrl;
 
-    public Theme (String springUrl, String summerUrl, String autumnUrl, String winterUrl) {
+    public Theme (String name, String springUrl, String summerUrl, String autumnUrl, String winterUrl) {
         this.id = UUID.randomUUID().toString().substring(0,8);
+        this.name = name;
         this.springUrl = springUrl;
         this.summerUrl = summerUrl;
         this.autumnUrl = autumnUrl;
