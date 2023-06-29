ddc4969219a2ae637360a225a74ccbf18bb2d900
josh <joshuastolle445@gmail.com>
2023-06-29 10:20:51 +0100

    create ThemeRpo with getList

--- backend/src/main/java/de/neuefische/backend/ThemeRepo.java
+++ backend/src/main/java/de/neuefische/backend/ThemeRepo.java
@@ -1,4 +1,21 @@
 package de.neuefische.backend;
 
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class ThemeRepo {
+
+    private final List<Theme> themes;
+
+    public ThemeRepo() {
+        this.themes = new ArrayList<>();
+    }
+
+    public List<Theme> getThemes() {
+        return themes;
+    }
+
 }
