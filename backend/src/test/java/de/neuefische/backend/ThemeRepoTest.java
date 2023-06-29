1edc97b5315f6af8c13f381bfcc502bb567c2974
josh <joshuastolle445@gmail.com>
2023-06-29 10:29:34 +0100

    add ThemeRepo getThemes test

--- backend/src/test/java/de/neuefische/backend/ThemeRepoTest.java
+++ backend/src/test/java/de/neuefische/backend/ThemeRepoTest.java
@@ -1,4 +1,27 @@
 package de.neuefische.backend;
 
-public class ThemeRepoTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+
+class ThemeRepoTest {
+
+
+    ThemeRepo themeRepo = mock(ThemeRepo.class);
+
+    @Test
+    void test_getThemes() {
+        //GIVEN
+        List<Theme> expected = new ArrayList<>();
+        //WHEN
+        List<Theme> actual = themeRepo.getThemes();
+        //THEN
+        Assertions.assertEquals(expected, actual);
+    }
+
+
 }
