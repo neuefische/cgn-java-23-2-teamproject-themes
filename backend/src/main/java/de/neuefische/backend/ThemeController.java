b93ee50d63acc06188845cc46203bb842d31c598
josh <joshuastolle445@gmail.com>
2023-06-29 11:19:39 +0100

    Changes by debby

--- backend/src/main/java/de/neuefische/backend/ThemeController.java
+++ backend/src/main/java/de/neuefische/backend/ThemeController.java
@@ -1,4 +1,26 @@
 package de.neuefische.backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RequestMapping("/api")
+@RestController
 public class ThemeController {
+
+    private final ThemeService themeService;
+
+    @Autowired
+    public ThemeController(ThemeService themeService) {
+        this.themeService = themeService;
+    }
+
+    @GetMapping("/theme")
+    public List<Theme> getThemes(){
+        return this.themeService.getThemes();
+    }
 }
