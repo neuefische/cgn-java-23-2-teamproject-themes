5daa78ebe9e971dbb48d03788708eef735374b8f
josh <joshuastolle445@gmail.com>
2023-06-29 10:49:13 +0100

    added ThemeServiceTest test_getThemes()

--- backend/src/main/java/de/neuefische/backend/ThemeService.java
+++ backend/src/main/java/de/neuefische/backend/ThemeService.java
@@ -1,4 +1,19 @@
 package de.neuefische.backend;
 
+
+import lombok.Data;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Data
 public class ThemeService {
+
+private final ThemeRepo themeRepo;
+
+    public List<Theme> getThemes() {
+        return themeRepo.getThemes();
+    }
+
 }
--- backend/src/test/java/de/neuefische/backend/ThemeServiceTest.java
+++ backend/src/test/java/de/neuefische/backend/ThemeServiceTest.java
@@ -1,4 +1,25 @@
 package de.neuefische.backend;
 
-public class ThemeServiceTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+
+class ThemeServiceTest {
+
+    ThemeService themeService = mock(ThemeService.class);
+
+    @Test
+    void test_getThemes() {
+        //GIVEN
+        List<Theme> expected = new ArrayList<>();
+        //WHEN
+        List<Theme> actual = themeService.getThemes();
+        //THEN
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
