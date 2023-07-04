7bff8a3c43c670e2351369fa82ac437401f2fd16
Enrico <e.weissenrieder94@gmail.com>
2023-07-04 13:04:57 +0200

    added delete request, frontend button &  backend logic

--- frontend/src/components/DisplayTheme.tsx
+++ frontend/src/components/DisplayTheme.tsx
@@ -1,13 +1,15 @@
 import {Theme} from "../utils/types.ts"
 import SeasonToggle from "./SeasonToggle.tsx"
 import {useState} from "react";
+import axios from "axios";
 
 type Props = {
     theme: Theme
+    setThemes: React.Dispatch<React.SetStateAction<Theme[]>>
 }
 
 
-export default function DisplayTheme({theme}: Props) {
+export default function DisplayTheme({theme, setThemes}: Props) {
 
     const [season, setSeason] = useState<string>(theme.seasonStatus);
 
@@ -26,10 +28,20 @@ export default function DisplayTheme({theme}: Props) {
         }
     }
 
+
+    function handleDelete() :void {
+        axios.delete(`/api/theme/${theme.id}`)
+            .then(response => response.data)
+            .catch(console.error)
+            .then((data) => {
+                setThemes(data);});
+    }
+
     return (
         <>
         <img width="200px" height="200px" src={getCurrentSeasonImageUrl(theme, season)} alt="Theme image"/>
         <SeasonToggle season={season} setSeason={setSeason} theme={theme} />
+            <button onClick={handleDelete}>DELETE THEME</button>
     </>
     )
 
