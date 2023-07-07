47b4f7510c7badb06d5be8bf2ebbee4a495c9c9e
Enrico <e.weissenrieder94@gmail.com>
2023-07-07 15:24:23 +0200

    added prev/next theme buttons

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,6 +1,5 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import DisplayTheme from "./DisplayTheme.tsx";
-import AddTheme from "./AddTheme.tsx";
 import {useFetch} from "../hooks/useFetch.ts";
 
 function Home() {
@@ -8,14 +7,44 @@ function Home() {
     const themes = useFetch((state) => state.themes);
     const fetchThemes = useFetch((state) => state.fetchThemes);
 
+    const [currentThemeIndex, setCurrentThemeIndex] = useState<number>(0);
+
     useEffect(() => {
         fetchThemes();
     }, [fetchThemes]);
 
+    function changeThemeIndex(chevronDirection: "increment" | "decrement") {
+        switch (chevronDirection) {
+            case "increment":
+                if (currentThemeIndex === themes.length - 1) {
+                    setCurrentThemeIndex(0);
+                }
+                else{
+                    setCurrentThemeIndex(currentThemeIndex + 1);
+                }
+                return
+            case "decrement":
+                if (currentThemeIndex === 0) {
+                    setCurrentThemeIndex(themes.length - 1);
+                }
+                else{
+                    setCurrentThemeIndex(currentThemeIndex - 1);
+                }
+                return
+        }
+    }
+
+
+    if (themes.length === 0) {
+        return null;
+    }
     return (
         <>
-            {themes.length === 0 ? null : themes.map(theme => <DisplayTheme key={theme.id} theme={theme}/>)}
-            <AddTheme/>
+            {currentThemeIndex}
+            <DisplayTheme key={currentThemeIndex + "theme"} theme={themes[currentThemeIndex]}/>
+
+            <button onClick={() => changeThemeIndex("decrement")}>{"<"}</button>
+            <button onClick={() => changeThemeIndex("increment")}>{">"}</button>
         </>
     );
 }
