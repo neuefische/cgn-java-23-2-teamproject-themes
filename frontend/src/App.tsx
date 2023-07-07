0556fe620cb32f80b38a1d9369b3cf9148d56830
Enrico <e.weissenrieder94@gmail.com>
2023-07-07 15:39:14 +0200

    add Global Styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Home from "./components/Home.tsx";
+import GlobalStyle from "./GlobalStyle.ts";
 
 function App() {
 
 
   return (
     <>
+        <GlobalStyle />
       <Home />
     </>
   )
