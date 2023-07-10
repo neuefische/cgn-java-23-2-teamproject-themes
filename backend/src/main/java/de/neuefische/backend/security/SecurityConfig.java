9345ecb9fe98c5a62b24cc7057b8bb1c23073edf
Josh <joshuastolle445@gmail.com>
2023-07-10 15:29:36 +0200

    add SecurityFilterChain

--- backend/src/main/java/de/neuefische/backend/security/SecurityConfig.java
+++ backend/src/main/java/de/neuefische/backend/security/SecurityConfig.java
@@ -2,11 +2,15 @@ package de.neuefische.backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
@@ -21,6 +25,23 @@ public class SecurityConfig {
                 .roles("BASIC")
                 .build()
         );
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
+        return http
+            .csrf().disable()
+            .httpBasic(Customizer.withDefaults())
+            .authorizeHttpRequests(httpRequests ->
+                httpRequests
+                    .requestMatchers(HttpMethod.GET,"/api/theme").permitAll()
+                    .requestMatchers("/api/theme").authenticated()
+                    .requestMatchers(HttpMethod.GET,"/api/theme/**").permitAll()
+                    .requestMatchers("/api/theme/**").authenticated()
+            )
+            .formLogin(Customizer.withDefaults())
+            .build();
+
     }
 
     @Bean
