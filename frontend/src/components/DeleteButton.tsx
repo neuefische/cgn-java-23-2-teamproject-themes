710292650db258b79771d5ba3e6dc3c503a108f5
Josh <joshuastolle445@gmail.com>
2023-07-10 20:02:28 +0200

    add edit button and position buttons

--- frontend/src/components/DeleteButton.tsx
+++ frontend/src/components/DeleteButton.tsx
@@ -6,23 +6,29 @@ type Props = {
     theme: Theme
 }
 
-export default function DeleteTheme({theme}: Props) {
+export default function DeleteButton({theme}: Props) {
 
     const deleteTheme = useFetch((state) => state.deleteTheme);
 
-    return (<DeleteButton onClick={() => deleteTheme(theme.id)}>
+    return (<Button onClick={() => deleteTheme(theme.id)}>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30px" height="30px" fill="red">
             <title>close-circle</title>
             <path
                 d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z"/>
         </svg>
-    </DeleteButton>)
+    </Button>)
 }
 
-const DeleteButton = styled.button`
+const Button = styled.button`
   width: 36px;
   height: 36px;
   border-radius: 5px;
   border: none;
   background-color: var(--colorBlack);
+  margin: 0;
+  padding: 0;
+  display: grid;
+  place-items: center;
+  cursor: pointer;
+  
 `;
--- frontend/src/pages/Gallery.tsx
+++ frontend/src/pages/Gallery.tsx
@@ -1,10 +1,9 @@
 import {Theme} from "../utils/types.ts"
-import {getCurrentSeasonImageUrl, getSeasonMainColor} from "../utils/utils.ts";
 import styled from "styled-components";
 import Header from "../components/Header.tsx";
-
 import {useFetch} from "../hooks/useFetch.ts";
-import DeleteTheme from "../components/DeleteTheme.tsx";
+import ThemeCard from "../components/ThemeCard.tsx";
+
 
 export default function Gallery() {
 
@@ -15,13 +14,7 @@ export default function Gallery() {
             <Header/>
             <StyledMain>
                 {themes.map((theme: Theme) =>
-                    <ThemeCard key={theme.id}>
-                        <ThemeContainer seasonstatus={theme.seasonStatus}>
-                            <ThemeName>{theme.name}</ThemeName>
-                            <Img src={getCurrentSeasonImageUrl(theme)} alt="theme"/>
-                            <DeleteTheme theme={theme}/>
-                        </ThemeContainer>
-                    </ThemeCard>)}
+                    <ThemeCard key={theme.id} theme={theme}/>)}
                 <div style={{height: "100px"}}/>
             </StyledMain>
         </>
@@ -32,45 +25,5 @@ const StyledMain = styled.main`
   margin-top: 20px;
 `;
 
-// refactor to own component
-const ThemeCard = styled.div`
-  display: grid;
-  place-items: center;
-  background: var(--colorWhite);
-`;
 
-type TypeThemeContainer = {
-    seasonstatus: string;
-}
 
-const ThemeContainer = styled.div<TypeThemeContainer>`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 320px;
-  height: 360px;
-  border-radius: 5px;
-  background: ${({seasonstatus}) => getSeasonMainColor(seasonstatus)};
-  margin: 12px;
-`;
-
-const ThemeName = styled.h2`
-  font-family: var(--font2);
-  color: var(--colorGrey);
-  text-align: center;
-  font-size: 20px;
-  font-style: normal;
-  font-weight: 300;
-  line-height: normal;
-  margin-top: 0;
-`;
-
-const Img = styled.img`
-
-  width: 280px;
-  height: 280px;
-  border-radius: 5px;
-  box-shadow: var(--shadow1);
-  
-`;
