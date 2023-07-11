951a7ecc562600250a60e173f667ddddc9dbfde6
Josh <joshuastolle445@gmail.com>
2023-07-11 12:30:42 +0200

    add CSRF token handling

--- backend/src/main/java/de/neuefische/backend/security/SecurityConfig.java
+++ backend/src/main/java/de/neuefische/backend/security/SecurityConfig.java
@@ -9,21 +9,29 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName(null);
+
         return http
-            .csrf().disable()
+            .csrf(csrf -> csrf
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .csrfTokenRequestHandler(requestHandler))
             .httpBasic(Customizer.withDefaults())
             .authorizeHttpRequests(httpRequests ->
                 httpRequests
-                    .requestMatchers(HttpMethod.GET,"/api/theme").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/api/theme").permitAll()
                     .requestMatchers("/api/theme").authenticated()
-                    .requestMatchers(HttpMethod.GET,"/api/theme/**").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/api/theme/**").permitAll()
                     .requestMatchers("/api/theme/**").authenticated()
                     .requestMatchers("/api/user/me").authenticated()
                     .anyRequest().authenticated()
