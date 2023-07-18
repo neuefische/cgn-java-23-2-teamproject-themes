4c176f98e67ff82f49c13393af887ebb922c9a07
Enrico <e.weissenrieder94@gmail.com>
2023-07-18 17:13:39 +0200

    fixed code smells

--- backend/src/main/java/de/neuefische/backend/security/MongoUserDetailsService.java
+++ backend/src/main/java/de/neuefische/backend/security/MongoUserDetailsService.java
@@ -12,7 +12,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
